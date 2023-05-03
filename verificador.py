8463f5659f19cbe04b0e34b3723e2e01105bb102
luisda190519 <licerol@uninorte.edu.co>
2023-05-03 14:53:21 -0500

    Added

--- verificador.py
+++ verificador.py
@@ -4,7 +4,18 @@ if __name__ == "__main__":
     filename = sys.argv[1]
     decompressed_filename = "descomprimido-elmejorprofesor.txt"
 
-    with open(filename, "r") as archivo1, open(decompressed_filename, "r") as archivo2:
+    try:
+      ENCODING = 'utf-8'
+      with open(filename, "r", encoding=ENCODING) as archivo1, open(decompressed_filename, "r", encoding=ENCODING) as archivo2:
+          for linea1, linea2 in zip(archivo1, archivo2):
+              if linea1 != linea2:
+                  print("nok")
+                  break
+          else:
+              print("ok")
+    except:
+      ENCODING = 'cp1252'
+      with open(filename, "r", encoding=ENCODING) as archivo1, open(decompressed_filename, "r", encoding=ENCODING) as archivo2:
         for linea1, linea2 in zip(archivo1, archivo2):
             if linea1 != linea2:
                 print("nok")
